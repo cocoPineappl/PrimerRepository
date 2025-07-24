d084ef46c39de6625b21fd0738f0517027b1b523
coconut <ignacioherrerareyes28@gmail.com>
2025-07-23 22:16:49 -0600

    6th commit,UpgrTo1.0.5

---
+++ code000/adv_calc.py
@@ -0,0 +1,21 @@
+# comment
+# Creado el: 07  @Author coconutPineappl
+# îî  version: 1.0.0  îî
+#  Calculadora de Rad, Log, +- de vectores y matrices,
+#  
+# Util para consultar 
+# 
+
+from delay_general import *
+import numpy as np
+import sympy as sp
+
+def mostrar_menu():
+    print("\n"+"="*40)
+    print("CALCULADORA DE BACHILLERATO GUAY".center(40))
+    print("1. Ecuacion Lineal")
+    print("2. Ecuacion Cuadratica")
+    print("3. Matrices (+,-,*)")
+    print("0. Salir")
+    print("="*40)
+    
